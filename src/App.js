060e074aa1fc32ec1571bdd6ffc789dc5c9de9a5
Falguni Pathak <falguni13.fgp@gmail.com>
2025-10-22 22:08:08 +0400

    Fix App.js: remove duplicate Routers and leftover merge text

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
+// src/App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import LandingPage from './components/LandingPage';
 import MeetingWindow from './components/MeetingWindow';
 import PreMeetingSetup from './components/PreMeetingSetup';
 import Login from './components/Login';
-feature/host-ui
 import Host from './components/Host';
 import DocumentUpload from './components/DocumentUpload';
 import { ClassroomProvider } from './context/ClassroomContext';
-main
-import './App.css';
+// import './App.css';
 
 export default function App() {
   return (
-feature/host-ui
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/meeting" element={<MeetingWindow />} />
-          <Route path="/pre-meeting" element={<PreMeetingSetup />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/meeting" element={<MeetingWindow />} />
-          <Route path="/pre-meeting" element={<PreMeetingSetup />} />
-          <Route path='/host' element={<Host/>} />
-        </Routes>
-      </div>
-    </Router>
-
     <ClassroomProvider>
       <Router>
         <div className="App">
@@ -36,10 +21,10 @@ feature/host-ui
             <Route path="/pre-meeting" element={<PreMeetingSetup />} />
             <Route path="/login" element={<Login />} />
             <Route path="/upload" element={<DocumentUpload />} />
+            <Route path="/host" element={<Host />} />
           </Routes>
         </div>
       </Router>
     </ClassroomProvider>
- main
   );
 }
