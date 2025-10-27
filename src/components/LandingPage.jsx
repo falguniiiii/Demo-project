d06b76790dff7a097307b91668c92669226fd8ec
Falguni Pathak <falguni13.fgp@gmail.com>
2025-10-27 20:39:41 +0400

    removed temp btn

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -159,26 +159,6 @@ const LandingPage = () => {
       >
         Join Meeting
       </button>
-
-      {/* temp btn-1 */}
-      <button onClick={() => navigate('/upload')} 
-       style={{
-          position: 'fixed',
-          right: 16,
-          bottom: 90,
-          zIndex: 2000,
-          padding: '12px 20px',
-          background: '#111',
-          color: '#fff',
-          borderRadius: 9999,
-          border: 'none',
-          boxShadow: '0 8px 20px rgba(0,0,0,.25)',
-          cursor: 'pointer',
-          fontWeight: 600,
-          fontSize: '14px'
-        }} className="get-started-btn">
-  Get started →
-</button>
     </div>
   );
 };
